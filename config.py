8457ef1d925c1341dc73f8b9a2e07c5ea192e831
ariarobot <yusufpremium2011@gmail.com>
2025-02-23 04:47:38 +0530

    All Done By Spark Developer 🧑🏻‍💻

---
+++ config.py
@@ -0,0 +1,19 @@
+# Made By Spark Developer
+#All Credit Goes to @Spark_Developer
+
+from os import getenv
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "")
+LOG_GROUP = getenv("LOG_GROUP", "")
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", ""))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", ""))
+WEBSITE_URL = getenv("WEBSITE_URL", "")
+AD_API = getenv("AD_API", "")
+STRING = getenv("STRING", None)
+YT_COOKIES = getenv("YT_COOKIES", None)
+INSTA_COOKIES = getenv("INSTA_COOKIES", None)
